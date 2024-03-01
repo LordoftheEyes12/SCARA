882d9d7cd04debdbb4d9f8ca4b6234159042c523
Ben Daschner <114725735+LordoftheEyes12@users.noreply.github.com>
2024-03-01 23:05:57 +0100

    added first inverse kinematics to the project

--- include/Robot.hpp
+++ include/Robot.hpp
@@ -1,4 +1,4 @@
-bool homeMotor(int stp, int dir, int end);
+bool homeMotor(Motor motor, int end);
 
 int prepPins(void);
 
@@ -8,4 +8,38 @@ typedef struct Motor
 {
     int step_pin;
     int dir_pin;
+    int uebersetzung;
+};
+
+typedef enum status_robot
+{
+    STANDING_STILL,
+    MOVING,
+    ERROR
+};
+
+typedef struct targetAngles
+{
+    int base;
+    int arm;
+    int z_axis;
+};
+
+int homingProcedure(void);
+
+void calculateAngle(double L1, double L2, double X, double Y, double &angle1_deg, double &angle2_deg);
+
+void inverseKinematics(double X_current, double Y_current, double X_target, double Y_target, double L1, double L2, double &delta_theta1, double &delta_theta2);
+
+typedef struct length
+{
+    double L1;
+    double L2;
+};
+
+typedef struct Position
+{
+    int x;
+    int y;
+    int z;
 };
--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,7 +1,11 @@
 #include "Robot.hpp"
 #include <Arduino.h>
-bool homeMotor(int stp, int dir, int end)
+#include <math.h>
+length Arm = {200, 150};
+bool homeMotor(Motor motor, int end)
 {
+    int stp = motor.step_pin;
+    int dir = motor.dir_pin;
     digitalWrite(dir, LOW);
     while (!digitalRead(end))
     {
@@ -25,9 +29,12 @@ int prepPins(void)
     return 0;
 }
 
-bool moveDeg(int deg, int stp, int dir)
+bool moveDeg(Motor motor, int deg)
 {
-    float steps_fl = (float)((1200 * deg) / 360);
+    int stp = motor.step_pin;
+    int dir = motor.dir_pin;
+    int multiplier = motor.uebersetzung;
+    float steps_fl = (float)((1200 * deg * multiplier) / 360);
     if (steps_fl < 0)
     {
         digitalWrite(dir, LOW);
@@ -46,4 +53,49 @@ bool moveDeg(int deg, int stp, int dir)
         delayMicroseconds(1000);
     }
     return true;
+
 }
+
+    void calculateAngle(double L1, double L2, double X, double Y, double &angle1_deg, double &angle2_deg)
+    {
+        double XY_dist = sqrt(X * X + Y * Y);
+
+        // Law of cosines
+        double angle1 = atan2(Y, X) - acos((L1 * L1 + XY_dist * XY_dist - L2 * L2) / (2 * L1 * sqrt(XY_dist * XY_dist)));
+        double angle2 = M_PI - acos((L1 * L1 + L2 * L2 - XY_dist * XY_dist) / (2 * L1 * L2));
+
+        // Convert angles to degrees
+        angle1_deg = angle1 * (180.0 / M_PI);
+        angle2_deg = angle2 * (180.0 / M_PI);
+    }
+
+    // Function to calculate the change in angles from current to target positions
+    void inverseKinematics(double X_current, double Y_current, double X_target, double Y_target, double L1, double L2, double &delta_theta1, double &delta_theta2)
+    {
+        double theta1_current, theta2_current;
+        double theta1_target, theta2_target;
+
+        // Calculate angles for current position
+        calculateAngle(L1, L2, X_current, Y_current, theta1_current, theta2_current);
+
+        // Calculate angles for target position
+        calculateAngle(L1, L2, X_target, Y_target, theta1_target, theta2_target);
+
+        // Calculate the change in angles
+        delta_theta1 = theta1_target - theta1_current;
+        delta_theta2 = theta2_target - theta2_current;
+    }
+
+    int movetoPos(Position currPos, Position targetPos, Motor motor1, Motor motor2)
+    {
+        double delta_theta1, delta_theta2;
+
+        // Calculate the change in angles
+        inverseKinematics(currPos.x, currPos.y, targetPos.x, targetPos.y, Arm.L1, Arm.L2, delta_theta1, delta_theta2);
+
+        // Move the motors to the new position
+        moveDeg(motor1, delta_theta1);
+        moveDeg(motor2, delta_theta2);
+
+        return 0;
+    }
